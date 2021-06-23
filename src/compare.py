491f8f1ed51d9f0ab04d76fcab8ce438e7c218de
Sara <sluisdejesus@gmail.com>
2021-06-23 21:20:39 +0100

    initial commit

---
+++ src/compare.py
@@ -0,0 +1,7 @@
+def compare(number_1,number_2):
+    if number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+    elif number_1 == number_2:
+        return "both numbers are the same"
+    else:
+        return f"{number_1} is greater than {number_2}"
